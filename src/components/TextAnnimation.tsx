438794a734e33706ec012beee8c64bbeb0c1cf2f
nibhapr <nibhapr@gmail.com>
2024-09-10 08:52:18 +0530

    ne build

--- src/components/TextAnnimation.tsx
+++ src/components/TextAnnimation.tsx
@@ -1,51 +1,40 @@
 import { motion } from "framer-motion";
+import Container from "./container.astro";
+import { useState, useEffect } from "react";
 
 const TextAnimation = () => {
 	const text =
 		"We're excited to showcase our products at DIDAC INDIA on 18-09-2024 at NEWDELHI.";
 
-	// Variants for staggered text animation
 	const container = {
 		hidden: { opacity: 0 },
+
 		visible: {
 			opacity: 1,
 			transition: {
-				staggerChildren: 0.05, // Time between each letter animation
+				staggerChildren: 0.05,
 			},
 		},
 	};
 
 	const letter = {
-		hidden: { opacity: 0, y: 50 }, // Start off-screen below
+		hidden: { opacity: 0, y: 50 },
 		visible: {
 			opacity: 1,
-			y: 0, // Animate to its original position
+			y: 0,
 			transition: { duration: 0.5, ease: "easeOut" },
 		},
 	};
 
 	return (
 		<motion.div
-			className="text-container"
+			className="md:flex justify-center text-red-600 px-8 text-base md:text-xl lg:text-2xl"
 			variants={container}
 			initial="hidden"
 			animate="visible"
-			style={{
-				display: "flex",
-				justifyContent: "center",
-				fontSize: "34px",
-				color: "red",
-			}}
 		>
 			{text.split("").map((char, index) => (
-				<motion.span
-					key={index}
-					variants={letter}
-					style={{
-						color: char === "DIDAC INDIA" ? "red" : "#dfgdg", // Color specific words or letters
-						display: "inline-block",
-					}}
-				>
+				<motion.span key={index} variants={letter}>
 					{char === " " ? "\u00A0" : char}
 				</motion.span>
 			))}
